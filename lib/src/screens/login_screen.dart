e9a692ce6bd92606cb9167e93aee8c9151a3d96a
Zeeshan Syed <zsyed89@gmail.com>
2019-04-19 17:50:54 -0400

    Created single global instance bloc

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ));
+  }
+}
+
+Widget emailField() {
+  return StreamBuilder(
+    stream: bloc.email,
+    builder: (context, snapshot) {
+      // Every time stream changes, builder functions re-runs, returns a new widget, and that new widget is displayed on the device
+      return TextField(
+        // Send value into email stream controller's sink
+        onChanged: bloc.changeEmail,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'you@example.com',
+          labelText: 'Email Address',
+          errorText: snapshot.error,
+        ),
+      );
+    },
+  );
+}
+
+Widget passwordField() {
+  return StreamBuilder(
+    stream: bloc.password,
+    builder: (context, snapshot) {
+      return TextField(
+        onChanged: bloc.changePassword,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          labelText: 'Password',
+          errorText: snapshot.error,
+        ),
+      );
+    },
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('Login'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
+}
